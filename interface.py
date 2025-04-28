48b2170c6bfb75c0802cbfbf75b42f6c694494e9
Michelle McAveety <135155059+mcaveety@users.noreply.github.com>
2025-04-28 15:54:53 -0400

    Initial commit

---
+++ interface.py
@@ -0,0 +1,5 @@
+# User Interface
+# Based on Tkinter functionality
+
+import tkinter
+
---
+++ main.py
@@ -0,0 +1,15 @@
+# MAS 3105 - Section 004
+# Spring 2025 Final Project - Error Correction Codes
+
+import hamming, reedmuller, reedsolomon
+import binascii
+import functions
+import interface
+
+def __main__():
+    print("MAS 3105 Final Project")
+    print(functions.get_message())
+
+
+if __name__ == "__main__":
+    __main__()
